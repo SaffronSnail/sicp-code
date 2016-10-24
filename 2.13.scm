dcfb097184ee96b338276fed012b4122162530c7
saffronsnail <saffronsnail@hushmail.com>
2016-10-24 12:28:24 -0700

    defined par1 and par2 as in the book (pg 131)

--- 2.13.scm
+++ 2.13.scm
@@ -1,7 +1,17 @@
+(load "2.7.scm")
+(load "2.10.scm")
+(load "2.11.scm")
+
 (define one-as-interval (cons-interval-vals 1 1))
 (define (inverse-interval i) (divide-interval one-as-interval i))
 
-(define (parallel-equivalent i1 i2)
+(define (par1 i1 i2)
+	(divide-interval (multiply-interval i1 i2)
+									 (add-interval      i1 i2)
+	)
+)
+
+(define (par2 i1 i2)
 	(inverse-interval
 		(add-interval (inverse-interval i1)
 									(inverse-interval i2)
