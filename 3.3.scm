a7712e312ee0aebcae68c1401f26a0770ea00000
saffronsnail <saffronsnail@hushmail.com>
2016-12-20 08:59:28 -0800

    added implementation of 3.3.scm

---
+++ 3.3.scm
@@ -0,0 +1,29 @@
+(define (make-account password balance)
+
+  (define (withdraw amount)
+    (if (>= balance amount)
+    (begin (set! balance (- balance amount))
+    balance)
+    "Insufficient funds")
+  )
+
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance
+  )
+
+  (define (dispatch given-password m)
+    (if (eq? given-password password)
+      (begin
+        (cond ((eq? m 'withdraw) withdraw)
+          ((eq? m 'deposit) deposit)
+          (else (error "Unknown request: MAKE-ACCOUNT" m))
+        )
+      )
+      (error "Bad password!")
+    )
+  )
+
+  dispatch
+)
+
